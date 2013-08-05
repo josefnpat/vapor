df43d88db147e336a2b90fb07b6d91e3dc8a3a10
Josef Patoprsty <seppi@josefnpat.com>
2013-08-05 01:22:57 -0400

    adding downloading icons

--- src/core/icons.lua
+++ src/core/icons.lua
@@ -5,4 +5,10 @@ icons.view = love.graphics.newImage("assets/icons/applications-internet.png")
 icons.download = love.graphics.newImage("assets/icons/emblem-web.png")
 icons.favorite = love.graphics.newImage("assets/icons/help-about.png")
 
+icons.downloading = {}
+icons.downloading[1] = love.graphics.newImage("assets/icons/network-idle.png")
+icons.downloading[2] = love.graphics.newImage("assets/icons/network-receive.png")
+icons.downloading[3] = love.graphics.newImage("assets/icons/network-transmit.png")
+icons.downloading[4] = love.graphics.newImage("assets/icons/network-transmit-receive.png")
+
 return icons
