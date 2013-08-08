josefnpat/vapor

commit f10787974abb3a3d931db8e9228bc0d0ef80e836

Adding qaisjp's lost merge changes for src/core/async.lua

src/core/async.lua
@@ -112,7 +112,10 @@ local socket_queue_mt = {
           if type(status) == "table" then
             status = unpack(status)
           end
-          error("Failed to resume coroutine: " .. tostring(status) .. "\n" .. debug.traceback(co))
+          error(("Failed to resume coroutine: %s\n%s"):format(
+            tostring(status),
+            debug.traceback(co)
+          ))
         end
 
         if status == "timeout" then
@@ -191,7 +194,11 @@ local function love_filesystem_sink(fname,download_hash)
         local sizemb,time = #data/(1024^2),stop-start
         local mbps = sizemb/time
 
-        print( round(mbps,4) .. " MB/s ("..round(sizemb,4).." MB in ".. round(time,4) .. " s)")
+        print(("%s MB/s (%s MB in  s)"):format(
+          round(mbps,4),
+          round(sizemb,4),
+          round(time,4)
+        ))
         
         love.filesystem.write(fname..".sha1",hash)
         print(fname .. " hashed.")

commit 23f3139563dd2725e4aecb44b8527bbfe506b3c1

Fixing log info bug introduced in f10787974abb3a3d931db8e9228bc0d0ef80e836

src/core/async.lua
@@ -194,7 +194,7 @@ local function love_filesystem_sink(fname,download_hash)
         local sizemb,time = #data/(1024^2),stop-start
         local mbps = sizemb/time
 
-        print(("%s MB/s (%s MB in  s)"):format(
+        print(("%s MB/s (%s MB in %s s)"):format(
           round(mbps,4),
           round(sizemb,4),
           round(time,4)