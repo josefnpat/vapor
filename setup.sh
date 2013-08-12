153cfec0ab71de6e16c032e1ab9f057725d3f6c2
Josef Patoprsty <seppi@josefnpat.com>
2013-08-12 10:21:56 -0400

    Adding clone version for loveframes-gray-theme

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-LOC=src/lib/loveframes
-git clone --depth 1 https://github.com/NikolaiResokav/LoveFrames.git $LOC
-cd $LOC
+LF=src/lib/loveframes
+git clone --depth 1 https://github.com/NikolaiResokav/LoveFrames.git $LF
+cd $LF
 git checkout a095495209baa385145a7eb8a730a9ae163ac6d1 # 0.9.6.1 Alpha
 cd -
 
-git clone --depth 1 https://github.com/UnekPL/loveframes-gray-theme.git $LOC/skins/gray
+LFGT=$LF/skins/gray
+git clone --depth 1 https://github.com/UnekPL/loveframes-gray-theme.git $LFGT
+cd $LFGT
+git checkout 9430d22020b9d0cb71f82c098e676eb5596e8b17
+cd -
+
