8d771be7acadde48de37cc2f43a282c167be4919
Josef Patoprsty <seppi@josefnpat.com>
2013-08-12 15:35:34 -0400

    Derp, kinda need this for the build.sh script.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,19 @@ cd $LFGT
 git checkout 9430d22020b9d0cb71f82c098e676eb5596e8b17
 cd -
 
+# Download binaries, and unpack
+
+cd dev/build_data
+VERSION=0.8.0
+
+# windows 32 bit
+wget -t 2 -c https://bitbucket.org/rude/love/downloads/love-$VERSION\-win-x86.zip
+unzip love-$VERSION\-win-x86.zip
+
+# windows 64 bit
+wget -t 2 -c https://bitbucket.org/rude/love/downloads/love-$VERSION\-win-x64.zip
+unzip love-$VERSION\-win-x64.zip
+
+# os x universal
+wget -t 2 -c https://bitbucket.org/rude/love/downloads/love-$VERSION\-macosx-ub.zip
+unzip love-$VERSION\-macosx-ub.zip
