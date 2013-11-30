f540a80cf30082d3dee09e81d66b163bfde53a12
Josef Patoprsty <seppi@josefnpat.com>
2013-11-30 15:13:53 -0500

    Change sort to ignore non-alphanumeric characters and games that start with "The "

--- src/core/remote.lua
+++ src/core/remote.lua
@@ -3,6 +3,21 @@ remote = {}
 remote.uri = "http://50.116.63.25/public/vapor/games.json"
 remote.file = "games.json"
 
+function remote.en_sort(a,b)
+  -- Lower case the input
+  local a_f,b_f = string.lower(a.name),string.lower(b.name)
+  -- Remove "the"
+  if string.sub(a_f,1,4) == "the " then
+    a_f = string.sub(a_f,5,-1)
+  end
+  if string.sub(b_f,1,4) == "the " then
+    b_f = string.sub(b_f,5,-1)
+  end
+  -- remove all non-alphanumeric characters
+  a_f,b_f = string.gsub(a_f,'%W',''),string.gsub(b_f,'%W','')
+  return a_f < b_f
+end
+
 -- @param force_local don't check remote server for new file
 function remote.load(force_local)
   
@@ -25,7 +40,7 @@ function remote.load(force_local)
     remote.data = json.decode(raw)
   end
 
-  table.sort(remote.data.games, function(a,b) return a.name:lower() < b.name:lower() end )
+  table.sort(remote.data.games,remote.en_sort)
 
 end
 
