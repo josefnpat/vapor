6517cfaa2885b6f71402c4671d1527a234c3947c
= <=>
2014-01-10 13:26:03 -0600

    added spanish language

---
+++ src/core/lang/es.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- Maintainer: Fayer
+  -- Internal
+
+  id = "ES",
+  name = "Español",
+  
+  -- Tabs
+  
+  list_all = "Todos",
+  list_all_tooltip = "Todos los juegos (%d)",
+  list_favorite = "Favoritos",
+  list_favorite_tooltip = "Tus juegos favoritos",
+  list_downloaded = "Descargados",
+  list_downloaded_tooltip = "Juegos Descargados",
+  list_new = "Nuevo",
+  list_new_tooltip = "Juegos añadidos recientemente (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Preferencias",
+  settings_tooltip = "Configura Vapor",
+  
+  -- Info Section
+  
+  size = "Tamaño",
+  welcome = "Bienvenido a Vapor.",
+  nodescription = "No hay descripción disponible.",
+  
+  -- Info Button Status
+
+  download = "Descarga",
+  download_tooltip = "Descargar ahora.",
+  downloading = "Descargando ...",
+  downloading_tooltip = "Tu juego se está descargando.",
+  hashing = "Verificando ...",
+  hashing_tooltip = "Tu juego se está verificando.",
+  invalid = "Error",
+  invalid_tooltip = "Hubo un error. Haz click para borrar este juego.",
+  play = "Jugar",
+  play_tooltip = "Este juego está listo para jugar.",
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Marcar como favorito.",
+  delete_tooltip = "Borar este juego.",
+  website_tooltip = "Visitar la web del autor."
+
+}
