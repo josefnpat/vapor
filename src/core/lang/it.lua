67b0ee0b408c3c526a901942e4416e1948c70d56
Nixola <niki.or97@yahoo.it>
2014-01-10 16:17:05 +0100

    Adding Italian language
    Initial translation, better get rid of that "hashing"

---
+++ src/core/lang/it.lua
@@ -0,0 +1,49 @@
+return {
+
+  -- Internal
+
+  id = "IT",
+  name = "Italiano",
+  
+  -- Tabs
+  
+  list_all = "Tutti",
+  list_all_tooltip = "Tutti i giochi (%d)",
+  list_favorite = "Preferiti",
+  list_favorite_tooltip = "I tuoi preferiti",
+  list_downloaded = "Scaricati",
+  list_downloaded_tooltip = "Giochi scaricati",
+  list_new = "Novità",
+  list_new_tooltip = "Giochi aggiunti di recente (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Impostazioni",
+  settings_tooltip = "Configura Vapor",
+  
+  -- Info Section
+  
+  size = "Dimensione",
+  welcome = "Benvenuto in Vapor.",
+  nodescription = "Nessuna descrizione disponibile.",
+  
+  -- Info Button Status
+
+  download = "Scarica",
+  download_tooltip = "Scarica questo gioco.",
+  downloading = "Scaricamento ...",
+  downloading_tooltip = "Il gioco sta venendo scaricato.",
+  hashing = "Hashando ..." --have to change this
+  hashing_tooltip = "Creando un hash del gioco.", --have to change this
+  invalid = "Errore",
+  invalid_tooltip = "C'è stato un errore. Clicca per eliminare il gioco.",
+  play = "Gioca",
+  play_tooltip = "Clicca per giocare a questo gioco." ,--have to change this
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Aggiungi questo gioco tra i preferiti.",
+  delete_tooltip = "Elmina questo gioco.",
+  website_tooltip = "Visita il sito dell'autore."
+
+}
