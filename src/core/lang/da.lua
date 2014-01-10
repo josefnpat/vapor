b5da4c7c4ceecaf1ebec864188d7aa44b6f9095a
josefnpat <josef.patoprsty@redbottledesign.com>
2014-01-10 11:57:39 -0500

    Add danish lang Closes #161

---
+++ src/core/lang/da.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- Maintainer: Gustav Geisler (jjmafiae)
+  -- Internal
+
+  id = "DA",
+  name = "Dansk",
+
+  -- Tabs
+
+  list_all = "Alle",
+  list_all_tooltip = "Alle spil (%d)",
+  list_favorite = "Favoritter",
+  list_favorite_tooltip = "Dine favorit spil",
+  list_downloaded = "Downloadede",
+  list_downloaded_tooltip = "Downloadede spil",
+  list_new = "Nye",
+  list_new_tooltip = "Nyeligt insættede spil (%d)",
+
+  -- Global Buttons
+
+  settings = "Indstillinger",
+  settings_tooltip = "Indstil Vapor",
+
+  -- Info Section
+
+  size = "Størrelse",
+  welcome = "Velkommen til Vapor.",
+  nodescription = "Intet resumé tilgængeligt",
+
+  -- Info Button Status
+
+  download = "Download",
+  download_tooltip = "Download dette spil nu.",
+  downloading = "Downloader ...",
+  downloading_tooltip = "Dit spil bliver downloadet",
+  hashing = "Tjekker ...",
+  hashing_tooltip = "Dit spil bliver tjekket.",
+  invalid = "Fejl",
+  invalid_tooltip = "Der er sket en fejl. Klik for at slette spil.",
+  play = "Spil",
+  play_tooltip = "Dette spil er klar til at blive spillet",
+
+  -- Game Buttons
+
+  favorite_tooltip = "Put det her spil på dine favoritter.",
+  delete_tooltip = "Slet det her spil.",
+  website_tooltip = "Besøg udviklerens hjemmeside."
+
+}
