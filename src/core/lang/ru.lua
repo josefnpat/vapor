219edfc5c80f259349683837048cbd6ab2dedf3a
josefnpat <josef.patoprsty@redbottledesign.com>
2014-01-10 11:58:36 -0500

    Add Russian lang Closes #163

---
+++ src/core/lang/ru.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- Maintainer: Insweater
+  -- Internal
+
+  id = "RU",
+  name = "Pусский",
+  
+  -- Tabs
+  
+  list_all = "Все",
+  list_all_tooltip = "Все игры (%d)",
+  list_favorite = "Избранные",
+  list_favorite_tooltip = "Ваши избранные игры",
+  list_downloaded = "Загруженные",
+  list_downloaded_tooltip = "Загруженные игры",
+  list_new = "Новые",
+  list_new_tooltip = "Недавно добавленные игры (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Настройки",
+  settings_tooltip = "Настроить Vapor",
+  
+  -- Info Section
+  
+  size = "Размер",
+  welcome = "Добро пожаловать в Vapor.",
+  nodescription = "Описание недоступно.",
+  
+  -- Info Button Status
+
+  download = "Загрузить",
+  download_tooltip = "Загрузить игру прямо сейчас.",
+  downloading = "Загружается ...",
+  downloading_tooltip = "Игра загружается.",
+  hashing = "Проверяется ...",
+  hashing_tooltip = "Игра проверяется.",
+  invalid = "Ошибка",
+  invalid_tooltip = "Произошла ошибка. Кликните, чтобы удалить игру.",
+  play = "Играть",
+  play_tooltip = "Игра готова к запуску.",
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Отметить эту игру как избранную.",
+  delete_tooltip = "Удалить эту игру.",
+  website_tooltip = "Посетить сайт автора."
+
+}
