458ba70e937fb77c7527dc6ec93a783568e67873
Yonaba <roland.yonaba@gmail.com>
2014-01-10 16:07:29 +0000

    Add language : French

---
+++ src/core/lang/fr.lua
@@ -0,0 +1,49 @@
+return {
+
+  -- Internal
+
+  id = "FR",
+  name = "Français",
+  
+  -- Tabs
+  
+  list_all = "Tout",
+  list_all_tooltip = "Tous les jeux (%d)",
+  list_favorite = "Favoris",
+  list_favorite_tooltip = "Vos jeux favoris",
+  list_downloaded = "Téléchargé",
+  list_downloaded_tooltip = "Jeux téléchargés",
+  list_new = "Nouveau",
+  list_new_tooltip = "Jeux ajoutés récemment (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Configuration",
+  settings_tooltip = "Configurer vapor",
+  
+  -- Info Section
+  
+  size = "Taille",
+  welcome = "Bienvenue dans Vapor.",
+  nodescription = "Aucune description disponible.",
+  
+  -- Info Button Status
+
+  download = "Télécharger",
+  download_tooltip = "Télécharger le jeu maintenant.",
+  downloading = "Téléchargement en cours ...",
+  downloading_tooltip = "Le jeu est en cours de téléchargement.",
+  hashing = "Vérification ...",
+  hashing_tooltip = "L'archive téléchargée est en cours de vérification ...",
+  invalid = "Erreur",
+  invalid_tooltip = "Une erreur a été rencontrée. Cliquez pour supprimer le contenu téléchargé.",
+  play = "Jouer",
+  play_tooltip = "Ce jeu est disponible.",
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Marquer ce jeu en tant que favori.",
+  delete_tooltip = "Supprimer le jeu.",
+  website_tooltip = "Voir le site web du développeur."
+
+}
