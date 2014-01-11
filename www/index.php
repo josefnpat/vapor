268f39f1d208a46ef6075d57d1ce717ef96862af
josefnpat <seppi@josefnpat.com>
2014-01-11 14:42:46 -0500

    Updating vapor website Closes #153

--- www/index.php
+++ www/index.php
@@ -8,39 +8,57 @@ $gamecount = count($data->games);
   <head>
     <title>Vapor &mdash; L&Ouml;VE Distribution Client</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="style.css.php">
   </head>
 
   <body>
+    <div class="wrapper">
 
-    <h1>Vapor</h1>
+      <div class="logo"><img src="logo.png" alt="Vapor" title="Vapor" /></div>
 
-    <h2>L&Ouml;VE Distribution Client</h2>
+      <h2>L&Ouml;VE Distribution Client</h2>
 
-    <h3><a href="https://github.com/josefnpat/vapor/releases/latest">Download the latest release of vapor here.</a></h3>
+      <p>Information</p>
 
-    <p>See the project at <a href="https://github.com/josefnpat/vapor">GitHub</a>.</p>
-    <p>Contribute to vapor! Check out the <a href="https://github.com/josefnpat/vapor/issues">issue queue</a>!</p>
-    <p>Vapor uses <a href="http://love2d.org">L&Ouml;VE 0.8.0</a>.</p>
+      <ul>
+        <li><a href="https://github.com/josefnpat/vapor/releases/latest">Download the latest release of Vapor</a>.</li>
+        <li>How to <a href="https://github.com/josefnpat/vapor#how-can-i-add-my-game">add your own L&Ouml;VE game</a> and the <a href="https://github.com/josefnpat/vapor#game-criteria">criteria required</a>.</li>
+        <li><a href="https://github.com/josefnpat/vapor#translations">Help translate Vapor</a>.</li>
+        <li>See the project at <a href="https://github.com/josefnpat/vapor">GitHub</a>.</li>
+        <li>Contribute to vapor! Check out the <a href="https://github.com/josefnpat/vapor/issues">issue queue</a>!</li>
+      </ul>
 
-    <h3>Games (<?php echo $gamecount; ?>)</h3>
+      <h3>Games (<?php echo $gamecount; ?>)</h3>
 
 <?php
-    usort($data->games,function($a,$b){ return $a->stable<$b->stable; });
+      usort($data->games,function($a,$b){ return $a->stable<$b->stable; });
 
-    echo "    <ul>\n";
-    foreach($data->games as $gamekey => $gamedata){
-      $name = $gamedata->name;
-      $date = date('Y-m-d',$gamedata->stable);
-      if(isset($gamedata->website)){
-        $author = '<a target="_blank" href="'.$gamedata->website.'">'.$gamedata->author."</a>";
-      } else {
-        $author = $gamedata->author;
+      foreach($data->games as $gamekey => $gamedata){
+        echo "<div class=\"game\">\n";
+        $image = "<img src=\"".$gamedata->image."\" />";
+        echo "<div class=\"info\">\n";
+        $name = $gamedata->name;
+        if(isset($gamedata->description)){
+          $desc = $gamedata->description;
+        } else {
+          $desc = "";
+        }
+        $date = date('Y-m-d',$gamedata->stable);
+        if(isset($gamedata->website)){
+          $author = '<a target="_blank" href="'.$gamedata->website.'">'.$gamedata->author."</a>";
+        } else {
+          $author = $gamedata->author;
+        }
+        echo "$image\n";
+        echo "<h3>$name</h3>\n";
+        echo "<p><em>$author</em> <small>($date)</small></p>\n";
+        echo "<p>$desc</p>\n";
+        echo "</div>\n";
+        echo "</div>\n";
       }
-    echo "      <li><b>$name</b> by <em>$author</em> <small>($date)</small></li>\n";
-    }
-    echo "    </ul>\n";
 
 ?>
 
+    </div><!-- end wrapper -->
   </body>
 </html>
---
+++ www/style.css.php
@@ -0,0 +1,74 @@
+<?php
+  header("Content-type: text/css; charset: UTF-8");
+
+  $color = array();
+  $color['black'] = "#000000";
+  $color['black_highlight'] = "#333333";
+
+  $color['blue'] = "#84c2ed";
+  $color['blue_highlight'] = "#a2c5f1";
+
+  $color['red'] = "#ed8484";
+  $color['red_highlight'] = "#fe9daa";
+  $color['red_reflection'] = "#fa836d";
+
+  $color['steel'] = "#5a5a5a";
+  $color['steel_highlight'] = "#7a7a7a";
+
+  $color['copper'] = "#5a4e3e";
+  $color['copper_highlight'] = "#e49455";
+
+?>
+
+@import url(http://fonts.googleapis.com/css?family=Open+Sans);
+
+body {
+  background-color: <?php echo $color['steel_highlight']; ?>;
+  color: <?php echo $color['blue']; ?>;
+  font-family: 'Open Sans', sans-serif;
+  padding: 0px;
+  margin: 0px;
+}
+
+a:link, a:active {
+  color: <?php echo $color['red']; ?>;
+}
+a:hover {
+  color: <?php echo $color['red_highlight']; ?>;
+}
+a:visited {
+  color: <?php echo $color['red_reflection']; ?>;
+}
+
+h1, h2, h3, h4 {
+  color: <?php echo $color['copper_highlight']; ?>;
+}
+
+.wrapper {
+  width: 920px;
+  padding: 0px 32px 32px;
+  margin: 0px auto;
+  background-color: <?php echo $color['steel']; ?>;
+}
+
+.logo {
+  background-color: <?php echo $color['black_highlight']; ?>;
+  width: 100%;
+  text-align: center;
+  padding: 0px;
+}
+
+.game {
+  height: 261px; /* 245 + 16 */
+  background-color: <?php echo $color['steel_highlight']; ?>;
+  margin: 8px 0px;
+}
+
+.game .info {
+  padding: 8px 0px 8px 16px;
+}
+
+.game img {
+  float: right;
+  padding: 0px 8px;
+}
