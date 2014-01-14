9c46b05aed185809b8b669d28a91c27ec5692858
BozoDel <danielbzd@zoho.com>
2014-01-14 13:22:14 -0200

    Add language Brazilian Portuguese

---
+++ src/core/lang/pt_br.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- Maintainer: BozoDel
+  -- Internal
+
+  id = "PT_BR",
+  name = "Português Brasileiro", -- I think there are enough differences between Brazilian and European Portuguese to justify this. 
+  
+  -- Tabs
+  
+  list_all = "Todos",
+  list_all_tooltip = "Todos os jogos (%d)",
+  list_favorite = "Favoritos",
+  list_favorite_tooltip = "Seus jogos favoritos",
+  list_downloaded = "Baixados",
+  list_downloaded_tooltip = "Jogos já baixados",
+  list_new = "Novos",
+  list_new_tooltip = "Jogos adicionados recentemente (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Configurações",
+  settings_tooltip = "Configurar o Vapor",
+  
+  -- Info Section
+  
+  size = "Tamanho",
+  welcome = "Bem-vindo ao Vapor.",
+  nodescription = "Nenhuma descrição disponível.",
+  
+  -- Info Button Status
+
+  download = "Baixar",
+  download_tooltip = "Baixar esse jogo agora.",
+  downloading = "Baixando...",
+  downloading_tooltip = "Seu jogo está sendo baixado.",
+  hashing = "Verificando...",
+  hashing_tooltip = "Seu jogo está sendo verificado.",
+  invalid = "Erro",
+  invalid_tooltip = "Aconteceu um erro. Clique para deletar esse jogo.",
+  play = "Jogar",
+  play_tooltip = "Esse jogo está pronto para ser jogado.",
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Marcar esse jogo como favorito.",
+  delete_tooltip = "Deletar esse jogo.",
+  website_tooltip = "Visitar o site do autor."
+
+}
