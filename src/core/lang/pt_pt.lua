bf42d5186f11c70fa9327ced66aac85450651572
Ranguna <ranguna313616@hotmail.com>
2014-01-15 18:13:04 +0000

    Added language European Portuguese

---
+++ src/core/lang/pt_pt.lua
@@ -0,0 +1,50 @@
+return {
+
+  -- Maintainer: Ranguna
+  -- Internal
+
+  id = "PT_PT",
+  name = "Português Europeu",
+  
+  -- Tabs
+  
+  list_all = "Todos",
+  list_all_tooltip = "Todos os jogos (%d)",
+  list_favorite = "Favoritos",
+  list_favorite_tooltip = "Os teus jogos favoritos",
+  list_downloaded = "Transferidos",
+  list_downloaded_tooltip = "Jogos transferidos",
+  list_new = "Novos",
+  list_new_tooltip = "Jogos recentemente adicionados (%d)",
+  
+  -- Global Buttons
+  
+  settings = "Definições",
+  settings_tooltip = "Configurar o Vapor",
+  
+  -- Info Section
+  
+  size = "Tamanho",
+  welcome = "Bem-vindo ao Vapor.",
+  nodescription = "Não existe nenhuma descrição.",
+  
+  -- Info Button Status
+
+  download = "Transferir",
+  download_tooltip = "Transferir este jogo agora.",
+  downloading = "A transeferir ...",
+  downloading_tooltip = "O teu jogo está a ser transferido.",
+  hashing = "A verificar ...",
+  hashing_tooltip = "O teu jogo esta a ser verificado.",
+  invalid = "Erro",
+  invalid_tooltip = "Houve um erro. Clica para apagar o jogo.",
+  play = "Jogar",
+  play_tooltip = "Este jogo está pronto para ser jogado.",
+  
+  -- Game Buttons
+  
+  favorite_tooltip = "Marcar este jogo como favorito.",
+  delete_tooltip = "Apagar este jogo.",
+  website_tooltip = "Visitar o site do autor."
+
+}
