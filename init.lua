4675f743687cf3cd6f7b7830b1cf5d068fa4b1fc
Seppi <seppi@josefnpat.com>
2015-04-09 18:05:54 -0400

    Add start to docs in ldoc format

--- init.lua
+++ init.lua
@@ -1,4 +1,23 @@
-local vapor = {}
+--- Vapor - LÖVE Distribution Client
+-- @module Vapor
+-- @author Josef N Patoprsty <seppi@josefnpat.com>
+-- @copyright 2015
+-- @license <a href="http://www.opensource.org/licenses/zlib-license.php">zlib/libpng</a>
+
+local vapor = {
+  _VERSION = "Vapor 1.x",
+  _DESCRIPTION = "LÖVE Distribution Client",
+  _URL = "http://vapor.love2d.org",
+  _LICENSE = [[
+    The zlib/libpng License
+    Copyright (c) 2015 Josef N Patoprsty
+    This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+    Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+    1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+  ]]
+}
 
 vapor.class = {
   game = require "gameclass",
