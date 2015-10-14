8d7e7b72ae69dae52b3a247e22356cb19627f9a3
Seppi <seppi@josefnpat.com>
2015-10-13 19:32:44 -0500

    Updating to match current 1.x of vapor

---
+++ conf.lua
@@ -0,0 +1,20 @@
+function love.conf(t)
+    t.identity = "vapor1.x"
+    t.version = "0.9.2"
+    t.console = false
+    t.window = nil
+    t.modules.audio = false
+    t.modules.event = true
+    t.modules.graphics = false
+    t.modules.image = false
+    t.modules.joystick = false
+    t.modules.keyboard = false
+    t.modules.math = false
+    t.modules.mouse = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.system = true
+    t.modules.timer = true
+    t.modules.window = false
+    t.modules.thread = true
+end
--- main.lua
+++ main.lua
@@ -1,33 +1,25 @@
-function love.update()
-  require("lovebird.lovebird").update()
+if not arg[2] then
+  print("usage:\n"..
+  "vapor p gameid\n"..
+  "vapor l\n")
 end
 
 vaporlib = require "vapor"
 
--- Find games lists
--- e.g. adds vapor.love2d.org/api & vapor.itch.io/api
--- This will also use the vapor.sources.local file as a fallback (first cache, then mount)
 vapor = vaporlib.new()
 
---vapor:getSourceCollection():add(
---  vapor.class.source.new({uri="vapor.sources.local"}))
---vapor:getSourceCollection():add(
---  vapor.class.source.new({uri="http://vapor.love2d.org/source"}))
 vapor:getMirrorCollection():add(
-  vapor.class.mirror.new({uri="http://api.myjson.com/bins/1qy6m"}):download()
+  vapor.class.mirror.new({uri="http://api.myjson.com/bins/18lrw"}):download()
 )
 
--- Download all sources. Failure is not an option!
-max_tries = 3
-
+max_tries = 5
 while not mirrors_done do
 
-  max_tries = max_tries - 1
   if max_tries < 0 then
-    print("Could not download mirrors")
+    print("[error] Mirror(s) could not be downloaded.")
     os.exit()
   else
-    print("Tries remaining #"..max_tries)
+    --print("Tries remaining #"..max_tries)
   end
 
   vapor:update()
@@ -39,70 +31,87 @@ while not mirrors_done do
       break
     end
     if mirror:getStatus() == vapor.status.fail then
-      print("Mirror failure.")
-      return
+      print("[error] Mirror has failed.")
+      os.exit()
     end
   end
 
-end
-
--- Search for requested game with id matching arg[1]
-local game = vapor:getGameObject(tonumber(arg[2]))
-
-if not game then
-  print("Game not found.")
-  return
-end
-
-game_stable = game:getRelease():getStableVersion()
-
-game:download()
-
-print("Downloading '"..game:getName().."'")
-print("Size: ",game_stable:getSize())
-print("Hash: ",game_stable:getHash())
-
-framework = vapor:getFrameworkObject(game:getFramework())
-
-framework:download()
-
-framework_stable = framework:getRelease():getStableVersion()
-
-if framework:getStatus() ~= vapor.status.ready then
-  print("Downloading '"..framework:getName().."'")
-  print("Size: "..framework_stable:getSize())
-  print("Hash: "..framework_stable:getHash())
-end
-
--- Spin until it's done downloading
-max_tries = 5
-
-while not done do
-
   max_tries = max_tries - 1
-  if max_tries < 0 then
-    print("Could not download game")
-    break
-  else
-    print("Tries remaining #"..max_tries)
-  end
-
-  vapor:update()
-
-  local game_status = game_stable:getStatus()
-  local framework_status = framework_stable:getStatus()
-
-  if game_status == vapor.status.ready and framework_status == vapor.status.ready then
-    game:play() -- ok, play the game!
-    done = true
-    print('[Game Played] SUCCESS')
-    love.event.quit()
-  elseif game_status == vapor.status.fail then
-    print("[Game Error] "..vapor.lang.translate("en",game_message))
-    done = true
-  elseif framework_status == vapor.status.fail then
-    print("[Framework Error] "..vapor.lang.translate("en",framework_message))
-    done = true
-  end
 
 end
+
+if arg[2] == "l" then
+
+  for _,g in pairs(vapor:getGameCollection():all()) do
+    print("game["..g:getIdentifier().."] "..g:getName())
+  end
+
+  for _,f in pairs(vapor:getFrameworkCollection():all()) do
+    print("framework["..f:getIdentifier().."] "..f:getName())
+  end
+
+
+elseif arg[2] == "p" then
+
+  -- Search for requested game with id matching arg[3]
+  local game = vapor:getGameObject(tonumber(arg[3]))
+
+  if not game then
+    print("[error] No game with id "..arg[3]..".")
+    os.exit()
+  end
+
+  game_stable = game:getRelease():getStableVersion()
+
+  game:download()
+
+  print("Targeting game '"..game:getName().."' ["..game:getIdentifier().."]")
+  print("Size: ",game_stable:getSize())
+  print("Hash: ",game_stable:getHash())
+
+  framework = vapor:getFrameworkObject(game:getFramework())
+
+  framework:download()
+
+  framework_stable = framework:getRelease():getStableVersion()
+
+  print("Targeting framework '"..framework:getName().."' ["..framework:getIdentifier().."]")
+  print("Size: ",framework_stable:getSize())
+  print("Hash: ",framework_stable:getHash())
+
+  max_tries = 5
+
+  while not done do
+
+    if max_tries < 0 then
+      print("[error] Game(s)/Framework(s) could not be downloaded.")
+      break
+    else
+      --print("Tries remaining #"..max_tries)
+    end
+
+    vapor:update()
+
+    local game_status = game_stable:getStatus()
+    local framework_status = framework_stable:getStatus()
+
+    if game_status == vapor.status.ready and framework_status == vapor.status.ready then
+      game:play() -- ok, play the game!
+      done = true
+      print('[Game Played] SUCCESS')
+    elseif game_status == vapor.status.fail then
+      print("[Game Error] "..vapor.lang.translate("en",game_message))
+      done = true
+    elseif framework_status == vapor.status.fail then
+      print("[Framework Error] "..vapor.lang.translate("en",framework_message))
+      done = true
+    end
+
+    max_tries = max_tries - 1
+
+  end
+else
+  print("unknown command: "..tostring(arg[2]))
+end
+
+love.event.quit()
